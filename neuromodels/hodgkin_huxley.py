60892bd6395d9da8ebd054812a8aefefbb885796
Nicolai Haug <nicolaihaug@yahoo.com>
2021-05-27 21:53:42 +0200

    Edit HH solver docs

--- neuromodels/hodgkin_huxley.py
+++ neuromodels/hodgkin_huxley.py
@@ -138,9 +138,9 @@ class HodgkinHuxley:
         :include-source: False
 
         import matplotlib.pyplot as plt
-        from pylfi.models import HodgkinHuxley
+        import neuromodels as nm
 
-        hh = HodgkinHuxley(V_rest=-70)
+        hh = nm.HodgkinHuxley(V_rest=-70)
         T = 50.
         dt = 0.025
 
@@ -494,33 +494,3 @@ class HodgkinHuxley:
             return self._h
         except AttributeError:
             raise ODEsNotSolved("Missing call to solve. No solution exists.")
-
-
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from pylfi.models import HodgkinHuxley
-
-    # Initialize the Hodgkin-Huxley system; model parameters can either be
-    # set in the constructor or accessed as class attributes:
-    hh = HodgkinHuxley(V_rest=-70)
-    hh.gbar_K = 36
-
-    # The simulation parameters needed are the simulation time ``T``, the time
-    # step ``dt``, and the input ``stimulus``, the latter either as a
-    # callable or ndarray with `shape=(int(T/dt)+1,)`:
-    T = 50.
-    dt = 0.025
-
-    def stimulus(t):
-        return 10 if 10 <= t <= 40 else 0
-
-    # The system is solved by calling the class method ``solve`` and the
-    # solutions can be accessed as class attributes:
-    hh.solve(stimulus, T, dt)
-    t = hh.t
-    V = hh.V
-
-    plt.plot(t, V)
-    plt.xlabel('Time [ms]')
-    plt.ylabel('Membrane potential [mV]')
-    plt.show()
