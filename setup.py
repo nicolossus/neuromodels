99011941c66c98d8f8d98ff732a6bcefe42d0c4e
Nicolai Haug <nicolaihaug@yahoo.com>
2021-06-01 00:37:37 +0200

    Add Python 3.9 to classifiers

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
