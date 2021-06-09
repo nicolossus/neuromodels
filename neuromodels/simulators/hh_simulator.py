423f05181e508b7dc7c7a85b2c412d7e8145e1c9
Nicolai Haug <nicolaihaug@yahoo.com>
2021-06-09 13:57:19 +0200

    Start on simulator model wrapper

---
+++ neuromodels/simulators/hh_simulator.py
@@ -0,0 +1,8 @@
+
+def hh_simulator():
+    # setattr() - send in param dict with values, **pdict,
+    # **kwargs to solve_ivp
+    # getattr()
+    # summary stat
+    # dense_output=False
+    pass
