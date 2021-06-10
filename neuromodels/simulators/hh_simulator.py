d3661262d47d863cf86fd401776e90b9cbd8197b
Nicolai Haug <nicolaihaug@yahoo.com>
2021-06-10 02:24:26 +0200

    Update docs

--- neuromodels/simulators/hh_simulator.py
+++ neuromodels/simulators/hh_simulator.py
@@ -1,4 +1,4 @@
-
+# must be class with call method that changes model parameters
 def hh_simulator():
     # setattr() - send in param dict with values, **pdict,
     # **kwargs to solve_ivp
@@ -6,3 +6,15 @@ def hh_simulator():
     # summary stat
     # dense_output=False
     pass
+
+    def plot_potential(self, ax=None):
+        pass
+
+    def plot_vtrace(self, show_state=False, show_stim=False):
+        # plt.plot(hh.t, hh.V)
+        if show_stim:
+            pass
+        pass
+
+    def plot_rates(self):
+        pass
